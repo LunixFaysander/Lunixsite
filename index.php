434d01e9810503b15f7e8e1199dd61ce7773d0a5
LunixFaysander <44734160+LunixFaysander@users.noreply.github.com>
2018-11-03 21:58:02 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,19 @@
-<!DOCTYPE html>
-<html>
-<body>
-
-<?php
-$ip = "127.0.0.1";
-
-if (!filter_var($ip, FILTER_VALIDATE_IP) === false) {
-    echo("$ip is a valid IP address");
-} else {
-    echo("$ip is not a valid IP address");
-}
-?>
-
-</body>
-</html>
+    <?php
+    /**
+     * Récupérer la véritable adresse IP d'un visiteur
+     */
+    function get_ip() {
+    	// IP si internet partagé
+    	if (isset($_SERVER['HTTP_CLIENT_IP'])) {
+    		return $_SERVER['HTTP_CLIENT_IP'];
+    	}
+    	// IP derrière un proxy
+    	elseif (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+    		return $_SERVER['HTTP_X_FORWARDED_FOR'];
+    	}
+    	// Sinon : IP normale
+    	else {
+    		return (isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : '');
+    	}
+    }
+    ?>
